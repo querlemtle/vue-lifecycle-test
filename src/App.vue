33762c708711237c258a0b98a643e9efd23fd94d
querlemtle <citta120120@gmail.com>
2022-08-19 01:40:42 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,114 @@
+<template>
+  <div id="app">
+    <h5>Hint: 請開啟 console 觀察</h5>
+    <header ref="header">
+      {{ header }}
+      <Nav :init-nav.sync="nav" />
+    </header>
+    <button class="update-btn" @click.prevent.stop="updateContent">
+      Update DOM
+    </button>
+    <button class="destroy-btn" @click.prevent.stop="destroyComponent">
+      Destroy component
+    </button>
+  </div>
+</template>
+
+<script>
+import Nav from "./components/Nav.vue";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      header: "old header",
+      nav: "old nav"
+    };
+  },
+  methods: {
+    updateContent() {
+      this.header = "new header";
+      this.nav = "new nav";
+    },
+    destroyComponent() {
+      this.$destroy();
+    },
+  },
+  components: {
+    Nav,
+  },
+  beforeCreate() {
+    console.log("<header> beforeCreate");
+    console.log("$el in beforeCreate: ", this.$el);
+    console.log("data in beforeCreate: ", this.$data);
+    console.log("<header> in beforeCreate: ", this.$refs.header);
+    console.log("----------------");
+  },
+  created() {
+    console.log("<header> created");
+    console.log("$el in created: ", this.$el);
+    console.log("data in created: ", this.$data);
+    console.log("<header> in created: ", this.$refs.header);
+    console.log("----------------");
+  },
+  beforeMount() {
+    console.log("<header> beforeMount");
+    console.log("$el in beforeMount: ", this.$el);
+    console.log("<header> in beforeMount: ", this.$refs.header);
+    console.log("----------------");
+  },
+  mounted() {
+    console.log("<header> mounted");
+    console.log("$el in mounted: ", this.$el);
+    console.log("<header> in mounted: ", this.$refs.header);
+    console.log("----------------");
+  },
+  beforeUpdate() {
+    console.log("<header> beforeUpdate");
+    console.log("$el in beforeUpdate: ", this.$el);
+    console.log("data in beforeUpdate: ", this.$data);
+    console.log("<header> in beforeUpdate: ", this.$refs.header);
+    console.log("----------------");
+  },
+  updated() {
+    console.log("<header> updated");
+    console.log("$el in updated: ", this.$el);
+    console.log("data in updated: ", this.$data);
+    console.log("<header> in updated: ", this.$refs.header);
+    console.log("----------------");
+  },
+  beforeDestroy() {
+    console.log("<header> beforeDestroy");
+    console.log("$el in beforeDestroy: ", this.$el);
+    console.log("<header> in beforeDestroy: ", this.$refs.header);
+    console.log("----------------");
+  },
+  destroyed() {
+    console.log("<header> destroyed");
+    console.log("$el in destroyed: ", this.$el);
+    console.log("data in destroyed: ", this.$data);
+    console.log("<header> in destroyed: ", this.$refs.header);
+    console.log("----------------");
+  },
+};
+</script>
+
+<style scoped>
+header {
+  padding: 0.5rem;
+  width: 50%;
+  height: 100px;
+  background-color: #cef3f3;
+}
+
+.update-btn {
+  margin-right: 1rem;
+}
+
+.destroy-btn {
+  margin-top: 1rem;
+  background-color: #ef233c;
+  border: 2px solid #a4161a;
+  color: #fff;
+}
+</style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,91 @@
+<template>
+  <nav ref="nav">{{ initNav }}</nav>
+</template>
+
+<script>
+export default {
+  name: "Nav",
+  props: {
+    initNav: {
+      type: String,
+    },
+  },
+  data() {
+    return {
+      nav: this.initNav,
+    };
+  },
+  computed: {
+    navHeader: {
+      get() {
+        return this.initNav;
+      },
+      set(value) {
+        // update nav title
+        this.$emit("update-nav", value);
+      },
+    },
+  },
+  beforeCreate() {
+    console.log("<nav> beforeCreate");
+    console.log("$el in beforeCreate: ", this.$el);
+    console.log("data in beforeCreate: ", this.$data);
+    console.log("<nav> in beforeCreate: ", this.$refs.nav);
+    console.log("----------------");
+  },
+  created() {
+    console.log("<nav> created");
+    console.log("$el in created: ", this.$el);
+    console.log("data in created: ", this.$data);
+    console.log("<nav> in created: ", this.$refs.nav);
+    console.log("----------------");
+  },
+  beforeMount() {
+    console.log("<nav> beforeMount");
+    console.log("$el in beforeMount: ", this.$el);
+    console.log("<nav> in beforeMount: ", this.$refs.nav);
+    console.log("----------------");
+  },
+  mounted() {
+    console.log("<nav> mounted");
+    console.log("$el in mounted: ", this.$el);
+    console.log("<nav> in mounted: ", this.$refs.nav);
+    console.log("----------------");
+  },
+  beforeUpdate() {
+    console.log("<nav> beforeUpdate");
+    console.log("$el in beforeUpdate: ", this.$el);
+    console.log("data in beforeUpdate: ", this.$data);
+    console.log("<nav> in beforeUpdate: ", this.$refs.nav);
+    console.log("----------------");
+  },
+  updated() {
+    console.log("<nav> updated");
+    console.log("$el in updated: ", this.$el);
+    console.log("data in updated: ", this.$data);
+    console.log("<nav> in updated: ", this.$refs.nav);
+    console.log("----------------");
+  },
+  beforeDestroy() {
+    console.log("<nav> beforeDestroy");
+    console.log("$el in beforeDestroy: ", this.$el);
+    console.log("<nav> in beforeDestroy: ", this.$refs.nav);
+    console.log("----------------");
+  },
+  destroyed() {
+    console.log("<nav> destroyed");
+    console.log("$el in destroyed: ", this.$el);
+    console.log("data in destroyed: ", this.$data);
+    console.log("<nav> in destroyed: ", this.$refs.nav);
+    console.log("----------------");
+  },
+};
+</script>
+
+<style scoped>
+nav {
+  width: 50%;
+  height: 50%;
+  background-color: #f2c1bb;
+}
+</style>
